b4898b3cf18a75fba7f2de341e2ce75fb3a7ca57
elmir <elmir@MSI>
2024-12-23 19:23:58 +0100

    Add project files.

---
+++ Backend/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("/HelloWorld")]
+    public class HelloWorldController : Controller
+    {
+        private readonly ILogger<HelloWorldController> _logger;
+        public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name ="GetHelloWorld")]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
