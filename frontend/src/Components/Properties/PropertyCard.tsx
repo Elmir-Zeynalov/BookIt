9f5f3cee9541e32745d4a830e6230b824b5a98da
Elmir-Zeynalov <elmirzeynalov1@gmail.com>
2025-01-08 18:09:43 +0100

    trying to add an image to the card

--- frontend/src/Components/Properties/PropertyCard.tsx
+++ frontend/src/Components/Properties/PropertyCard.tsx
@@ -1,6 +1,6 @@
 
 import { Property } from "./types";
-
+import propertyImage from '../../../Images/PropertyImages/0be04875-2131-49b7-ab6e-867378b82f32_IMG_1152.jpeg';
 interface PropertyProps {
     className: string,
     propertyInfo: Property;
@@ -10,7 +10,7 @@ interface PropertyProps {
 function PropertyCard({ className, propertyInfo }: PropertyProps) {
     return (
         <div style={{ border: "1px solid" }} >
-            <h1> IMAGE</h1>
+            <img src={`/images/PropertyImages/0e04875-2131-49b7-a6b6-867378b82f32_IMG_1152.jpeg`}   alt="Italian Trulli" />
             <h3>{propertyInfo.apartmentName}</h3>
             <p>{propertyInfo.apartmentLocation}</p>
             <p>{propertyInfo.leasing}</p>
