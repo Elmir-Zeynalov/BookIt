e0ddf42741fe0c5eb468e370980fb7952754d527
Elmir-Zeynalov <elmirzeynalov1@gmail.com>
2025-01-26 22:46:10 +0100

    adding a default empty string to authResult

--- Backend/Dtos/AuthenticationResult.cs
+++ Backend/Dtos/AuthenticationResult.cs
@@ -3,6 +3,6 @@
     public class AuthenticationResult
     {
         public bool IsSuccessful { get; set; } 
-        public string Message { get; set; }
+        public string Message { get; set; } = string.Empty;
     }
 }
